87b2c74f7f74c990d2f7009592a6a3fa6ed74887
baobeila <2912541860@qq.com>
2019-06-15 17:09:36 +0800

    图片转视频

---
+++ Imagetovideo.py
@@ -0,0 +1,34 @@
+#coding=utf-8
+#Version:python3.5.2
+import os
+import cv2
+import time
+# 图片合成视频
+def picvideo(path, size):
+
+    #D:\pycharm\Pycharmworkspace\gen_mnist1
+    filelist = os.listdir(path)  # 获取该目录下的所有文件名
+
+    '''
+    fps:
+    帧率：1秒钟有n张图片写进去[控制一张图片停留5秒钟，那就是帧率为1，重复播放这张图片5次] 
+    如果文件夹下有50张 534*300的图片，这里设置1秒钟播放5张，那么这个视频的时长就是10秒
+    每秒钟填充图像的帧数（帧/秒）
+    '''
+    fps = 10
+    # size = (640,480) #图片的分辨率
+    file_path = r"D:\pycharm\Pycharmworkspace\\" + str(int(time.time())) + ".avi"  # 导出路径
+    fourcc = cv2.VideoWriter_fourcc('D', 'I', 'V', 'X')  # 不同视频编码对应不同视频格式（例：'I','4','2','0' 对应avi格式）
+
+    video = cv2.VideoWriter(file_path, fourcc, fps, size)
+
+    for item in filelist:
+        if item.endswith('.jpg'):  # 判断图片后缀是否是.jpg
+            item = path + '/' + item
+            img = cv2.imread(item)  # 使用opencv读取图像，直接返回numpy.ndarray 对象，通道顺序为BGR ，注意是BGR，通道值默认范围0-255。
+            video.write(img)  # 把图片写进视频
+
+    video.release()  # 释放
+
+if __name__ == '__main__':
+    picvideo(r'D:\pycharm\Pycharmworkspace\gen_mnist1', (640, 480))
